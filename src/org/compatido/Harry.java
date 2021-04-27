bd4035bffb8c466168c3bdb509a3ef89321ad993
Alberto Garay <alberto@de.donde.sea>
2021-04-27 09:51:31 +0200

    Tercer commit de Sally

--- src/org/compatido/Harry.java
+++ src/org/compatido/Harry.java
@@ -1,7 +1,14 @@
 package org.compatido;
 
 public class Harry {
+	
+	
 	public static void main(String[] args) {
 		System.out.println("HARRY");
+	}
+	
+
+	private void sally() {
+		System.out.println("SALLY");
 	}
 }

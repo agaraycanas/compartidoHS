e0843747c66e41219cd64bd362f505eeff6c3ae9
Alberto Garay <alberto@de.donde.sea>
2021-04-27 09:49:42 +0200

    Tercer commit de Harry

--- src/org/compatido/Harry.java
+++ src/org/compatido/Harry.java
@@ -1,7 +1,12 @@
 package org.compatido;
 
 public class Harry {
+	
 	public static void main(String[] args) {
+		System.out.println("HARRY");
+	}
+	
+	private void harry() {
 		System.out.println("HARRY");
 	}
 }

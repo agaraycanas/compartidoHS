a2feb3bf1339c51934c33589e8cd9eacbb374dd0
Alberto Garay <alberto@de.donde.sea>
2021-05-13 09:45:53 +0200

    Más de JUnit

--- src/t8/ejercicios/ej01/Cadena.java
+++ src/t8/ejercicios/ej01/Cadena.java
@@ -2,7 +2,24 @@ package t8.ejercicios.ej01;
 
 public class Cadena {
 
-	public Cadena(String string) {
+	private String         cadena;
+	
+	public Cadena(String cadena) {
+		this.cadena = cadena;
 	}
+
+	public String getCadena() {
+		return this.cadena;
+	}
+
+	public void cDelante(String cadenaDelante) {
+		this.cadena = cadenaDelante + this.cadena;
+	}
+
+	public void cDetras(String cadenaDetras) {
+		this.cadena = this.cadena + cadenaDetras;
+	}
+	
+	
 
 }
--- test/t8/ejercicios/ej01/CadenaTest.java
+++ test/t8/ejercicios/ej01/CadenaTest.java
@@ -1,14 +1,41 @@
 package t8.ejercicios.ej01;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CadenaTest {
 
 	@Test
-	@DisplayName("Creo la cadena 'pepe' y vep que el obketo contiene exactamente eso")
+	@DisplayName("createGet01: Creo la cadena 'test' y veo que el objeto contiene exactamente eso")
 	public void createGet01() {
+
 		//ARRANGE
-		Cadena cadena = new Cadena("pepe");
+		Cadena cadena = new Cadena("test");
+		
+		//ACT
+		String stringObtenido = cadena.getCadena();
+		
+		//ASSERT
+		assertEquals("test", stringObtenido);
 	}
+
+	@Test
+	@DisplayName("cDelante01: 'latas' cDelante('abre') sale abrelatas")
+	public void cDelante01() {
+		Cadena cadena = new Cadena("latas");
+		cadena.cDelante("abre");
+		assertEquals("abrelatas", cadena.getCadena());
+	}
+
+	@Test
+	@DisplayName("cDetras01: 'corre' cDetras('caminos') sale correcaminos")
+	public void cDetras01() {
+		Cadena cadena = new Cadena("corre");
+		cadena.cDetras("caminos");
+		assertEquals("correcaminos", cadena.getCadena());
+	}
+
+	
 }

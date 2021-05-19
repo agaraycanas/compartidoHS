db763ba6defbe82a545e2e2cc717703919a64740
Alberto Garay <alberto@de.donde.sea>
2021-05-19 10:11:23 +0200

    Ejercicio 2 de JUnit

---
+++ src/t8/ejercicios/ej02/Utilidades.java
@@ -0,0 +1,17 @@
+package t8.ejercicios.ej02;
+
+import t8.ejercicios.ej02.exception.FechaException;
+
+
+public class Utilidades {
+	
+	/**
+	 * Informa de si un día determinado es finde semana o no.
+	 * Vida y milagros ,,,,,
+	 * 
+	 * @
+	 */
+	public boolean esFinDeSemana(int dia, int mes, int anyo) throws FechaException{
+		return true;
+	}
+}
